2fe73f0203da74d4591547ddf03742d5b521fa69
JFlakk <143786432+JFlakk@users.noreply.github.com>
2025-07-15 08:19:05 +0000

    Update

--- obj/Debug/net8.0/OneStream_Speed.AssemblyInfo.cs
+++ obj/Debug/net8.0/OneStream_Speed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneStream_Speed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c7ade549e2e18bdd808d7ed7e513fea206e2eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f9da8d54715b0eac6e7f082424ff4195303f0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneStream_Speed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneStream_Speed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
